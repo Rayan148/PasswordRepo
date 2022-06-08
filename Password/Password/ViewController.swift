c1f511de7b8183f9ee63355440448b63d801f86a
Rayan Shrestha <rayan.shrestha@ebpearls.com>
2022-06-08 17:48:52 +0545

    This is the password project. I have deleted the storyboard and I will build this project without using storyboard.

---
+++ Password/Password/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Password
+//
+//  Created by Ebpearls on 08/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
